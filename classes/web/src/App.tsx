384deb62ac0a3440b297c4c3f82dc38bb2db222a
Pedro Nandi <pedronandi@gmail.com>
2020-10-15 21:06:25 -0300

    Rocketseat NLW#3 - Aula 1 Construçao da landing page e da pagina do mapa.

---
+++ classes/web/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './styles/global.css';
+
+import Routes from './routes';
+import { Route } from 'react-router-dom';
+
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+  <h1>{props.text}</h1>
+  )
+}
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
